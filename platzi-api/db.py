fa0d62972415e48a5a141742b66c3de481cdd0bc
Diego Camino <diego.camino.r@gmail.com>
2020-05-13 02:20:45 -0500

    Ejecución de Queries

--- platzi-api/db.py
+++ platzi-api/db.py
@@ -42,3 +42,43 @@ def actualizar_carrera(carrera):
 def borrar_carrera_por_id(carrera_id):
     return str(db.carreras.delete_one({'_id': ObjectId(carrera_id)}))
 
+
+# Clase de operadores
+def consultar_carreras(skip, limit):
+    return dumps(db.carreras.find({}).skip(int(skip)).limit(int(limit)))
+
+
+def agregar_curso(json):
+    return str('Falta por implementar')
+
+
+def borrar_curso_de_carrera(json):
+    return str('Falta por implementar')
+
+# -----------------Cursos-------------------------
+
+
+def crear_curso(json):
+    return dumps(db.cursos.insert_one(json).inserted_id)
+
+
+def consultar_curso_por_id(id_curso):
+    return dumps(db.cursos.find_one({'_id': ObjectId(id_curso)}))
+
+
+def actualizar_curso(curso):
+    # Esta funcion solamente actualiza nombre, descripcion y clases del curso
+    return str(db.cursos.update_one({'_id': ObjectId(curso['_id'])}, {'$set': {'nombre': curso['nombre'], 'description': curso['description'], 'clases': curso['clases']}}).modified_count)
+
+
+def borrar_curso_por_id(curso_id):
+    return str(db.cursos.delete_one({'_id': ObjectId(curso_id)}).deleted_count)
+
+
+def consultar_curso_por_id_proyeccion(id_curso, proyeccion=None):
+    return str('Falta por implementar')
+
+
+def consultar_curso_por_nombre(nombre):
+    return str('Falta por implementar')
+
